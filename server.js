8114da6261f6637fc90324ab24eea1d0c49b6fd1
vishal modanwal <vk0764394@gmail.com>
2025-09-30 17:58:33 +0530

    database setup

---
+++ server.js
@@ -0,0 +1,18 @@
+import app from "./app.js";
+import { config } from "dotenv";
+import connectDB from "./config/db.js";
+
+config({path:'.env'});
+
+
+
+connectDB()
+.then(()=>{
+     app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`server is running at port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("mongodb connection failed", err);
+})
+
