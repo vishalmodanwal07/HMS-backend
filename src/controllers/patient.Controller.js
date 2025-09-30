b7fed9e319ab5a6ecd2abbb384475e36dd1273d7
vishal modanwal <vk0764394@gmail.com>
2025-10-01 04:38:41 +0530

    patient api done

--- src/controllers/patient.Controller.js
+++ src/controllers/patient.Controller.js
@@ -1,3 +1,93 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import {ApiResponse} from "../utils/apiResponse.js";
 import {ApiError} from "../utils/apiError.js";
+import { Patient } from "../models/patient.model.js";
+
+const createPatient = asyncHandler(async(req , res)=>{
+ const {name , age , gender , contact , assignedDoctor}=req.body;
+ if (!name || !gender || !contact) {
+    throw new ApiError(400, "Name, gender, and contact are required");
+  }
+   const patient = await Patient.create({
+    name,
+    age,
+    gender,
+    contact,
+    assignedDoctor: assignedDoctor || null,
+    createdBy: req.user._id
+  });
+  return res
+    .status(201)
+    .json(new ApiResponse(201, patient, "Patient created successfully"));
+});
+
+
+//list patients with pagination 
+const getPatients = asyncHandler(async(req , res)=>{
+    const { page = 1, limit = 10, q, doctor } = req.query;
+    const filter = {};
+  if (q) filter.$text = { $search: q };
+  if (doctor) filter.assignedDoctor = doctor;
+  const total = await Patient.countDocuments(filter);
+  const patients = await Patient.find(filter)
+    .populate("assignedDoctor", "name email")
+    .skip((page - 1) * limit)
+    .limit(Number(limit))
+    .sort({ createdAt: -1 });
+    return res
+    .status(200)
+    .json(new ApiResponse(200, { total, page, limit, patients }, "Patients fetched successfully"));
+});
+ 
+
+const   getPatientById = asyncHandler(async(req , res)=>{
+    const {id} = req.params.id;
+    const patient = await Patient.findById(id).populate("assignedDoctor", "name" , "email");
+    if (!patient) throw new ApiError(404, "Patient not found");
+    return res
+    .status(200)
+    .json(new ApiResponse(200, patient, "Patient fetched successfully"));
+});
+
+const  updatePatient = asyncHandler(async(req , res)=>{
+    const {id} =  req.params.id;
+    const { name, age, gender, contact, assignedDoctor } = req.body;
+    const patient = await Patient.findByIdAndUpdate(
+     id,
+    { name, age, gender, contact, assignedDoctor },
+    { new: true }
+  );
+  if (!patient) throw new ApiError(404, "Patient not found");
+  return res
+  .status(200)
+  .json(new ApiResponse(200, patient, "Patient updated successfully"));
+});
+
+const  assignDoctor = asyncHandler(async(req , res)=>{
+      const {id} = req.params.id;
+      const { doctorId } = req.body;
+      if (!doctorId) throw new ApiError(400, "DoctorId is required");
+       const patient = await Patient.findByIdAndUpdate(
+     id,
+    { assignedDoctor: doctorId },
+    { new: true }
+  );
+   if (!patient) throw new ApiError(404, "Patient not found");
+   return res
+   .status(200)
+   .json(new ApiResponse(200, patient, "Doctor assigned successfully"));
+});
+
+const  deletePatient = asyncHandler(async(req , res)=>{
+    const {id} = req.params.id;
+    const patient = await Patient.findByIdAndDelete(id);
+    if (!patient) throw new ApiError(404, "Patient not found");
+    return res.status(200).json(new ApiResponse(200, null, "Patient deleted successfully"));
+});
+
+export {  createPatient,
+  getPatients,
+  getPatientById,
+  updatePatient,
+  assignDoctor,
+  deletePatient}
--- src/routes/patientRoutes.js
+++ src/routes/patientRoutes.js
@@ -1,6 +1,17 @@
 import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { authorize } from "../middlewares/role.middleware.js";
+import { assignDoctor, createPatient, deletePatient, getPatientById, getPatients, updatePatient } from "../controllers/patient.Controller.js";
 
 const patientRouter = Router();
+
+patientRouter.route("/").post(verifyJWT , authorize("Reception", "Admin") , createPatient);
+patientRouter.route("/").get(verifyJWT , authorize("Reception", "Admin" , "Doctor") , getPatients);
+patientRouter.route("/:id").get(verifyJWT , authorize("Reception", "Doctor", "Lab", "Admin") , getPatientById);
+patientRouter.route("/:id").put(verifyJWT , authorize("Reception", "Admin") , updatePatient);
+patientRouter.route("/:id/assign-doctor").post(verifyJWT , authorize("Reception", "Admin") , assignDoctor);
+patientRouter.route("/:id").delete(verifyJWT , authorize("Admin") , deletePatient);
+
 
 
 export default patientRouter;
